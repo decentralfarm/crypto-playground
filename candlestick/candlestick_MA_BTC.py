f1d83698e7b860a1fbdcf2c15d23ca4e92cce159
Decentral Farm <decentfarm@protonmail.com>
2022-09-25 12:23:25 -0400

    Remove double fig

--- candlestick/candlestick_MA_BTC.py
+++ candlestick/candlestick_MA_BTC.py
@@ -13,6 +13,5 @@ fig = go.Figure(data=[go.Candlestick(
                                      name="Daily Candlestick"),
                                      ])
 
-fig.show()
 fig.add_trace(go.Scatter( y=data.MA200, line=dict(color='magenta', width=1), name="Moving Average 200"))
 fig.show()
